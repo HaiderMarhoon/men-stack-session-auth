15aca03c1a50ffdb286db1d605fb1aa5dec07646
HaiderMarhoon <haidooer2003@gmail.com>
2025-07-15 16:13:27 +0300

    sing in

--- controllers/auth.js
+++ controllers/auth.js
@@ -10,10 +10,10 @@ router.get("/" , (req,res) =>{
     res.send("Does is work?")
 })
 
+//sign up
 router.get("/sign-up" ,(req,res) =>{
     res.render("auth/sign-up.ejs")
 })
-
 
 //post to the db
 router.post("/sign-up", async(req,res) =>{
@@ -29,6 +29,23 @@ router.post("/sign-up", async(req,res) =>{
     req.body.password = hashedPassword
     const newUser = await User.create(req.body)
     res.send(`thanks for sign up ${newUser.username}`)
+})
+
+//sign in
+router.get("/sign-in" ,(req,res)=>{
+    res.render("auth/sign-in.ejs")
+}) 
+
+router.post("/sign-in",async(req,res)=>{
+    const userInDB = await User.findOne({username: req.body.username})
+    if(!userInDB) {
+        res.send("No username , need to sing-up")
+    }
+    const validPassword = bcrypt.compareSync(req.body.password, userInDB.password)
+    if(!validPassword){
+        res.send("Wrong Password, try again")
+    }
+    res.send ("Welcome")
 })
 
 
